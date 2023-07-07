37a3ad457dbc77753cc73f9d1c1d7e0d65c03ce1
nekonekko <momoichigodayo+github@gmail.com>
2023-07-07 18:05:35 +0900

    feat: :sparkles: plot sigmoid interpolation func

---
+++ plot_sigmoid_interpolation_func.R
@@ -0,0 +1,28 @@
+# rerturn survival rate
+sigmoid_interpolation_func <- function(x, a) {
+  y <- 1/2 * (1 + (1 - exp(-1 * a * (2 * x - 1))) / (1 + exp(-1 * a * (2 * x - 1))) * (1 + exp(-1 * a)) / (1 - exp(-1 * a)))
+  return (y)
+}
+
+x <- seq(0.01, 1, by=0.01)
+max_a <- 20
+for (a in 1:max_a) {
+  plot(
+    x,
+    sigmoid_interpolation_func(x, a), 
+    type='l',
+    xaxt='n',
+    yaxt='n', 
+    xlab='',
+    ylab=''
+  )
+  par(new=T)
+}
+
+axis(side=1, at=seq(0, 1.1, by=0.1))
+axis(side=2, at=seq(0, 2, by=0.1))
+title(
+  paste('sigmoid interpolation funcion (max_a=', max_a, ')'),
+  xlab='egg size', 
+  ylab='survival rate'
+)
